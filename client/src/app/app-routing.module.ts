4fc12186fe19bd25866601f06ba5deb36f964fcc
David S <eliasdavidsch@hotmail.com>
2023-05-07 18:22:35 -0500

    actualizado el routin

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProductComponent } from './pages/product/product.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NotfoundModule } from './notfount/notfound.module';
+import { Error404Component } from './notfount/error404/error404.component';
 
 const routes: Routes = [
-  
+  //pages
+  {path:"dashboard", component:DashboardComponent},
+  {path:"product", component:ProductComponent},
+  {path:"category", component:CategoryComponent},
+  //auth
+  {path:"login", component:LoginComponent},
+  {path:"register", component:RegisterComponent},
+  //notfound
+  {path:"**", component:NotfoundModule},
+
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes), CommonModule],
